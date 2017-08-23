d56eb5fd584bac9b33e6e126056346678ee4910f
Andriy Drozhko <a.drozhko@spd-ukraine.com>
2017-08-23 13:54:30 +0300

    refactor add web module move api to module

---
+++ web/src/main/java/com/reminderweb/reminderweb/ReminderWebApplication.java
@@ -0,0 +1,12 @@
+package com.reminderweb.reminderweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReminderWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReminderWebApplication.class, args);
+	}
+}
